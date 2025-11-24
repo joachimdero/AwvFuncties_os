de6c56f965524d3907892f0d472d0121048dff2f
derojp <joachim.dero@mow.vlaanderen.be>
2025-11-24 22:19:33 +0100

    installatie jwt

--- libs/AuthenticatieProxyAcmAwv.py
+++ libs/AuthenticatieProxyAcmAwv.py
@@ -24,7 +24,8 @@ Geef ofwel een cookie op, ofwel de een tuple van (key-file-path, cert-file-path)
 OpM: Proxies moeten met environment variables gezet worden
 
 """
-
+from qgis.core import (
+    QgsProcessingFeedback,)
 
 def ensure_module(package_name, import_name=None):
     feedback.pushInfo("ensure_module")
