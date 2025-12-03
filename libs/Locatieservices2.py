d2d6e54d3ad7202dac7a3787925d287ee94c8332
derojp <joachim.dero@mow.vlaanderen.be>
2025-12-03 19:37:07 +0100

    ls2

--- libs/Locatieservices2.py
+++ libs/Locatieservices2.py
@@ -3,7 +3,33 @@ import sys
 
 import Feedback
 
-Feedback.feedback_fn('test')
+F_TYPE = {
+    'refpunt_wegnr': ["TEXT", 10, "rfpntWnr"],
+    'refpunt_opschrift': ["DOUBLE", 0, "rfpntOpsch"],
+    'refpunt_afstand': ["LONG", 0, "rfpntAfst"],
+    'proj_x': ["DOUBLE", 0],
+    'proj_y': ["DOUBLE", 0],
+    'wsoidn': ["LONG", 0],
+    'Wsoidn': ["LONG", 0],
+    'wsoidn_m': ["DOUBLE", 0],
+    'begin_refpunt_wegnr': ["TEXT", 10, "bRpntWnr"],
+    'begin_refpunt_opschrift': ["DOUBLE", 0, "bRpntOpsch"],
+    'begin_refpunt_afstand': ["LONG", 0, "bRpntAfst"],
+    'begin_proj_x': ["DOUBLE", 0, "bProjX"],
+    'begin_proj_y': ["DOUBLE", 0, "bProjY"],
+    'begin_wsoidn': ["LONG", 0, "bWsoidn"],
+    'begin_wsoidn_m': ["DOUBLE", 0, "bWsoidnM"],
+    'VanM': ["DOUBLE", 0, "VanM"],
+    'eind_refpunt_wegnr': ["TEXT", 10, "eRpntWnr"],
+    'eind_refpunt_opschrift': ["DOUBLE", 0, "eRpntOpsch"],
+    'eind_refpunt_afstand': ["LONG", 0, "eRpntAfst"],
+    'eind_proj_x': ["DOUBLE", 0, "eProjX"],
+    'eind_proj_y': ["DOUBLE", 0, "eProjY"],
+    'eind_wsoidn': ["LONG", 0, "eWsoidn"],
+    'eind_wsoidn_m': ["DOUBLE", 0, "eWsoidnM"],
+    'TotM': ["DOUBLE", 0, "TotM"],
+    'wegnummer': ["TEXT", 10],
+}
 
 def request_ls2_puntlocatie(locaties, omgeving="apps", zoekafstand=2, crs=31370, session=None, gebruik_kant_van_de_weg='false', feedback=None):
     Feedback.feedback_fn('testbericht', feedback)
