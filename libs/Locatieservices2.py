8ccbbfddcc3bd718e86b4a042ef99e444049a2ad
derojp <joachim.dero@mow.vlaanderen.be>
2025-12-05 13:28:49 +0100

    ls2

--- libs/Locatieservices2.py
+++ libs/Locatieservices2.py
@@ -53,5 +53,5 @@ def request_ls2_puntlocatie(locaties, omgeving="apps", zoekafstand=2, crs=31370,
             Feedback.feedback_fn(f"probleem bij opvragen: status {response.status_code}", feedback)
             Feedback.feedback_fn(f'response:{str(response)[:200]}', feedback)
             Feedback.feedback_fn(f'jsonArgs:{jsonArgs[:200]}', feedback)
+            raise Exception(f"Fout bij opvragen van locatieservices2 puntlocatie: {response.status_code} {response.reason} {response.text}")
 
-        return None
